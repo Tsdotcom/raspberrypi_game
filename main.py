7534a1919ef7958ac00e7157202938a58a55da5e
Tsdotcom <venusvenus053@gmail.com>
2023-12-04 18:16:15 +0900

    third update

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
 import display
 import starting_activity
+import os
 disp = display.Display()
-main_game = starting_activity.Start()
+starting_gui = starting_activity.Start()
+# img_path = os.path("game_images/gameimages/1.jpeg")
 def main():
     # my_image = Image.new("RGB",(disp.width,disp.height))
     # my_draw = ImageDraw.Draw(my_image)
     # my_draw.rectangle((0,0,disp.width,disp.height),fill=(255,0,0,100))
     # disp.disp.image(my_image)
-    if main_game.root.start_game == False:
-        main_game.start_game()
+    if starting_gui.root.start_game == False:
+        starting_gui.start_game()
+    # image = Image.new("RGB", (disp.width, disp.height))
+    # draw = ImageDraw.Draw(image)
+    # draw.rectangle((0,0,disp.width, disp.height), fill=(255,0,0,100))
+    with Image.open("./game_images/gameimages/1.jpeg") as im:
+        im.rotate(45).show()
         
-    if main_game.root.end_game == True:
+    if starting_gui.root.end_game == True:
         pass
 
 if __name__ == '__main__':
--- starting_activity.py
+++ starting_activity.py
@@ -7,15 +7,28 @@ import display
 class Start:
     def __init__(self):
         self.root = Settings()
-        gui = tk.Tk()
-        
     def end_game(self):
-        pass
+        self.root.end_game = True
     def start_game (self):
         self.root.start_game = True
-
-
-
+    def start_gui(self):
+        self.start_root = Start()
+        self.gui = tk.Tk()
+        self.start_button_text = tk.StringVar()
+        self.start_button_text.set("START GAME")
+        self.end_button_text = tk.StringVar()
+        self.end_button_text.set("QUIT GAME")
+        self.background_style = ttk.Style()
+        self.background_style.configure('.',background='gray')
+        self.main_frame = ttk.Frame(self.gui,style="main_frame.TFrame",height= self.start_root.root.display_height, width=
+                            self.start_root.root.display_width)
+        self.start_button = ttk.Button(self.main_frame, text='START', style='start_button.Tbutton', textvariable=self.start_button_text, width = 15, command=self.start_root.start_game)
+        self.end_button = ttk.Button(self.main_frame,text='QUIT', style='quit_button.Tbutton', textvariable=self.end_button_text, width = 15, command=self.start_root.end_game() )
+        self.start_button.grid(row = 2, columnspan=3, pady=5)
+        self.end_button.grid(row = 4, columnspan= 3, pady= 5)
+        self.gui.mainloop()
+    
+    
 
     
 
